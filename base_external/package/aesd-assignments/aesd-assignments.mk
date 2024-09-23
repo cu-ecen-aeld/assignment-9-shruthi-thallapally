b497eb5917dd0d12afdbd106a3449248f1b08cb8
Shruthi Thallapally <shruthi.thallapally@colorado.edu>
2024-09-22 18:02:53 -0600

    after scp execution

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '86e1769bdf0381473abfd207a13d6fc0bbcf028e'
+AESD_ASSIGNMENTS_VERSION = 'd26bba68d4bb997282ceee22db1e0e5d77c90747'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
